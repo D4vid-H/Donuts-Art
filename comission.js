d4a3ddfadbe02ab091fc58fddbc3df48970a95d0
David Hillton <davidhillton@gmail.com>
2022-06-08 00:43:36 -0300

    full maquetado comissions

--- comission.js
+++ comission.js
@@ -5,11 +5,17 @@ const card = document.querySelector(".card");
 card.innerHTML = `
 <img class="cardImg" src="" alt="">
 <div class="cardTxt">
-    <p class="comissTittle">Brust</p>
-    <p class="comissContent">Character +10 Element +5 Background +10</p>
-    <p class="comissPrice">$ 20.00 USD</p>
+    <p class="comissTittle">${'Brust'}</p>
+    <p class="comissContent">${'Character +10 Element +5 Background +10'}</p>
+    <p class="comissPrice">${'$ 20.00 USD'}</p>
     <button class="btnQuote">Quote</button>
 </div>`
 ;
 
 }
+
+const modal = () => {
+
+
+
+ }
