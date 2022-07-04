e78195cdd0743808066640b2915fe657ef006ef7
bswalter8 <bswalter8@gmail.com>
2022-07-04 01:19:08 -0300

    Commit Walter Arreglo errores index

--- main.js
+++ main.js
@@ -488,22 +488,6 @@ const modalFunction = () => {
   };
 };
 
-/*const swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});*/
 
 function isLinkInViewport(elem) {
   let distance = elem.getBoundingClientRect();
